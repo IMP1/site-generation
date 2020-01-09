ea829cd92e888b2f5936d7bb9124105028ceb60f
IMP1 <IMP1@mail.com>
2020-01-09 10:36:20 +0000

    Ignore files found in .genignore

--- generator.rb
+++ generator.rb
@@ -4,6 +4,8 @@ require 'fileutils'
 require_relative 'rml'
 require_relative 'git_helpers'
 
+GENERATOR_IGNORE_FILENAME = ".genignore"
+
 class Generator
 
     def initialize(source_dir, target_dir, source_branch, target_branch)
@@ -11,8 +13,23 @@ class Generator
         @target_path = target_dir
         @source_branch = source_branch
         @target_branch = target_branch
-        @whitelist = []
-        @blacklist = []
+        @ignore_patterns = []
+        create_ignore_list
+        # TODO, have sync only be set to true if verbose flag is enabled
+        $stdout.sync = true 
+    end
+
+    def create_ignore_list
+        path = File.join(@source_path, GENERATOR_IGNORE_FILENAME)
+        if File.exists?(path)
+            File.readlines(path).each do |line|
+                @ignore_patterns.push(ignore_pattern_to_regex(line))
+            end
+        end
+    end
+
+    def ignore_pattern_to_regex(pattern)
+        return /#{pattern}/
     end
 
     def generate
@@ -29,29 +46,41 @@ class Generator
 
     def process_source_content
         Dir["**/*"].each do |filename|
-            # Filter out using whitelist and blacklist
-            next if File.directory?(File.join(@source_path, filename))
-            create_output_content(filename)
+            next if filename == GENERATOR_IGNORE_FILENAME
+            if @ignore_patterns.any? { |pattern| pattern.match?(filename) }
+                puts "Ignoring #{filename} as it matches a pattern in #{GENERATOR_IGNORE_FILENAME}."
+                next
+            end
+            filepath = File.join(@source_path, filename)
+            next if File.directory?(filepath)
+            if filename.end_with?(".rml")
+                puts "Converting #{source_filename}"
+                rml_content = File.read(filepath)
+                content = RMLParser.new(rml_content, filename).parse
+            else
+                puts "Copying #{source_filename}"
+                File.open(filepath, 'rb') { |f| content = f.read }
+            end
+            create_output_content(filename, content)
         end
     end
 
-    def create_output_content(source_filename)
-        puts "Converting #{source_filename}"
+    def create_output_content(source_filename, content)
         Dir.chdir(@target_path) do
             if @target_branch
                 commit_message = "Add #{source_filename}" # TODO: allow for custom message and custom message format
                 Git.checkout(@target_branch) do
-                    create_output_file(source_filename)
+                    create_output_file(source_filename, content)
                     Git.add(@target_path)
                     Git.commit(commit_message)
                 end
             else
-                create_output_file(source_filename)
+                create_output_file(source_filename, content)
             end
         end
     end
 
-    def create_output_file(source_filename)
+    def create_output_file(source_filename, content)
         target_filename = source_filename
         if source_filename.end_with?(".rml")
             target_filename = source_filename.gsub(".rml", ".html")
@@ -65,18 +94,14 @@ class Generator
 
         puts "Creating output file #{target_filename}"
         if source_filename.end_with? '.rml'
-            html_content = ""
-            Dir.chdir(@source_path) do
-                rml_content = File.read(source_filepath)
-                html_content = RMLParser.new(rml_content, source_filename).parse
-            end
-            
             File.open(target_filepath, 'w') do |file|
-                file.write(html_content)
+                file.write(content)
                 file.write("\n")
             end
         else
-            FileUtils.copy(source_filepath, target_filepath)
+            File.open(target_filepath, 'wb') do |file|
+                file.write(content)
+            end
         end
     end
 
