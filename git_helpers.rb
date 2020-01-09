d604714f7bcae1b1f25956bb1c4eb89d2217d88f
IMP1 <IMP1@mail.com>
2020-01-09 10:36:44 +0000

    Allow specifying repo to check for branch

--- git_helpers.rb
+++ git_helpers.rb
@@ -1,20 +1,30 @@
 module Git
 
-    def self.branch?(branch)
-        branches = run("git branch", false)
+    def self.branch?(branch, repo_dir=nil)
+        if repo_dir
+            Dir.chdir(repo_dir) do
+                return branch?(branch)
+            end
+        end
+        branches = `git branch`
         regex = Regexp.new('[\\n\\s\\*]+' + Regexp.escape(branch.to_s) + '\\n')
         result = ((branches =~ regex) ? true : false)
         return result
     end
 
-    def self.current_branch
-        branch = `git branch`
-        current_branch = branch.match(/\* (\w+)/)[1]
+    def self.current_branch(repo_dir=nil)
+        if repo_dir
+            Dir.chdir(repo_dir) do
+                return current_branch(branch)
+            end
+        end
+        branches = `git branch`
+        current_branch = branches.match(/\* (\w+)/)[1]
     end
 
-    def self.checkout(branch, &block)
+    def self.checkout(branch, file=nil, &block)
         if block.nil?
-            return `git checkout #{branch} --quiet`
+            return `git checkout #{branch} #{file || ""} --quiet`
         end
         previous_branch = current_branch
         checkout(branch)
